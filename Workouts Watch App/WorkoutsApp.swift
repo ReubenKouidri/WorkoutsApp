db2ceb00872b0d140571da851bbd5d78868960cf
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-10-18 13:53:39 +0100

    Initial Commit

---
+++ Workouts Watch App/WorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutsApp.swift
+//  Workouts Watch App
+//
+//  Created by Hamid Kouidri on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Workouts_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
